5be5dfbf06beecb1f64c71ac74ce526c7cf48f0f
Northium <northiumnetwork@gmail.com>
2024-09-16 01:48:39 +0100

    new distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://192.168.1.164/minecraft/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://192.168.1.164/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
